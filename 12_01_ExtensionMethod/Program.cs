a214144dc7bce859969181c86d8d209d6f10dedf
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2025-07-07 17:10:08 +0300

    Dictionary

---
+++ 12_01_ExtensionMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _12_01_ExtensionMethod
+{
+    static class ExampleExtension
+    {
+        public static int NumberWords(this string data)
+        {
+            if(string.IsNullOrEmpty(data)) return 0;
+            return data.Split(new char[] {' ',',','!'}, 
+                StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+        public static int NumberSymbol(this string data, char s)
+        {
+            if (string.IsNullOrEmpty(data)) return 0;
+            int c = 0;
+
+            foreach (var l  in data)
+            {
+                if (l == s) c++;
+            }
+            return c;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string str = "Lorem Ipsum is simply dummy text of the " +
+                "printing and typesetting industry. ";
+            Console.WriteLine("Count off word : " + str.NumberWords());
+            Console.WriteLine("Count off word : " + str.NumberSymbol('o'));
+
+        }
+    }
+}
---
+++ 13_GarbageCollector/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _13_GarbageCollector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //(Garbage Collector).
+
+            int a = 5;
+            string str = "Hello 1";
+            string copy = null;//null  " "
+            //Console.WriteLine(copy.Length);
+            if(true)
+            {
+                int b = 10;
+                string str2 = "Hello 2";
+                copy = str2;
+                Console.WriteLine(str2);
+            }
+            Console.WriteLine(str);
+            Console.WriteLine(copy);
+           // Console.WriteLine(str2);
+
+
+
+        }
+    }
+}
